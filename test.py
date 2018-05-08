1fc2b386d0897b4dec94bac3c31bcc94e8eb89d3
Kavya Elchuri <kelchuri@andrew.cmu.edu>
2018-05-07 21:09:09 -0400

    First commit-1

---
+++ test.py
@@ -0,0 +1,4 @@
+str = "{\"machineId\":1,\"temp\":33.600000,\"date\":\"2018-04-22 23:10:04\"}"
+str1 = "{\"machineId\":1,\"temp\":33.600000,\"date\":\"2018-04-22 23:10:04\"}"
+import json
+print(json.loads(str1))
